f866d7d7c2f5e714221fff3412e5b57d42ee92f2
pythonai250824 <pythonai250824@gmail.com>
2025-01-08 19:57:32 +0200

    upload

---
+++ insert_orders.sql
@@ -0,0 +1,10 @@
+INSERT INTO orders (customer_name, order_date) VALUES ('Jon Snow', '2025-01-01');
+INSERT INTO orders (customer_name, order_date) VALUES ('Daenerys Targaryen', '2025-01-02');
+INSERT INTO orders (customer_name, order_date) VALUES ('Arya Stark', '2025-01-03');
+INSERT INTO orders (customer_name, order_date) VALUES ('Tyrion Lannister', '2025-01-04');
+INSERT INTO orders (customer_name, order_date) VALUES ('Cersei Lannister', '2025-01-05');
+INSERT INTO orders (customer_name, order_date) VALUES ('Bran Stark', '2025-01-06');
+INSERT INTO orders (customer_name, order_date) VALUES ('Sansa Stark', '2025-01-07');
+INSERT INTO orders (customer_name, order_date) VALUES ('Jaime Lannister', '2025-01-08');
+INSERT INTO orders (customer_name, order_date) VALUES ('Sandor Clegane', '2025-01-09');
+INSERT INTO orders (customer_name, order_date) VALUES ('Brienne of Tarth', '2025-01-10');
